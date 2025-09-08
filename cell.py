8e450a3fa55da037f602f0b455015d56a0b8bef3
Nico9090 <161892855+Nico9090@users.noreply.github.com>
2025-09-08 19:15:11 -0400

    Update cell.py

--- cell.py
+++ cell.py
@@ -43,6 +43,71 @@ class Cell():
             organelle.draw(ax)
         plt.show()
 
+def plot_sphere(ax, radius, color="blue", alpha=0.3, position=(0,0,0)):
+    u = np.linspace(0, 2 * np.pi, 50)
+    v = np.linspace(0, np.pi, 50)
+
+    x = radius * np.outer(np.cos(u), np.sin(v)) + position[0]
+    y = radius * np.outer(np.sin(u), np.sin(v)) + position[1]
+    z = radius * np.outer(np.ones_like(u), np.cos(v)) + position[2]
+
+    ax.plot_surface(x, y, z, color=color, alpha=alpha, linewidth=0)
+
+def plot_ellipsoid(ax, radii=(5,5,5), center=(0,0,0), color="lightblue", alpha=0.3):
+    u = np.linspace(0, 2*np.pi, 50)
+    v = np.linspace(0, np.pi, 50)
+
+    x = radii[0] * np.outer(np.cos(u), np.sin(v)) + center[0]
+    y = radii[1] * np.outer(np.sin(u), np.sin(v)) + center[1]
+    z = radii[2] * np.outer(np.ones_like(u), np.cos(v)) + center[2]
+
+    ax.plot_surface(x, y, z, color=color, alpha=alpha, linewidth=0)
+
+
+class Cell():
+    def __init__(self):
+        self.cell = None
+        self.organelles:List[Organelle]=[]
+    def makeCell(self,innerRadius,outerRadius):
+        if outerRadius > innerRadius:
+            self.cell = {
+                "innerRadius":innerRadius,
+                "outerRadius":outerRadius,
+                "outerArea":np.pi*outerRadius**2,
+                "innerArea":np.pi*innerRadius**2,
+                "membraneArea": (np.pi*outerRadius**2) - (np.pi*innerRadius**2)
+                }
+        else:
+            return f"Error: outerRadius must be greater than innerRadius"
+        return self.cell
+    def addOrganelle(self,organelle):
+        if isinstance(organelle,Organelle):
+            self.organelles.append(organelle)
+            print(f"Added {organelle} to cell")
+        else:
+            print("Not a valid organelle")
+    def drawCell(self):
+        if not self.cell:
+            return f"Error: cell not created"
+        fig = plt.figure()
+        ax = fig.add_subplot(111,projection="3d")
+
+        plot_sphere(ax, self.cell["outerRadius"],
+                    color = "steelblue",
+                    alpha = 0.2)
+        plot_sphere(ax, self.cell["innerRadius"],
+                    color = "red",
+                    alpha = 0.3)
+        
+        for organelle in self.organelles:
+            organelle.draw(ax)
+        r = self.cell["outerRadius"]
+        ax.set_xlim([-r,r])
+        ax.set_ylim([-r,r])
+        ax.set_zlim([-r,r])
+        ax.set_box_aspect([1,1,1])
+
+        plt.show()
 
 
 
