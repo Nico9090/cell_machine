d3f6d983367b33d7383b4342ffb04074ced19a8d
Nico9090 <161892855+Nico9090@users.noreply.github.com>
2025-09-08 18:43:25 -0400

    Update organelle.py

--- organelle.py
+++ organelle.py
@@ -1,6 +1,20 @@
 from dataclasses import dataclass
+from typing import Optional
+import matplotlib.patches as patches
+import matplotlib.pyplot as plt
 @dataclass
-class Organelle:
-    name:str
-    number_of_chromosomes:float
+class Organelle():
+    name: str
+    shape: str
+    size: Optional[float]=None
+    radius: Optional[float]=None
+
+    def draw(self, ax, position=(0,0)):
+        if self.shape == "circle" and self.radius:
+            obj = plt.Circle(position,
+                             self.radius,
+                             color = "white")
+            ax.add_artist(obj)
+
+
     
