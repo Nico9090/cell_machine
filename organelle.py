34ab86f117b63e1cf0e1e7ac0715a51d25eb2889
Nico9090 <161892855+Nico9090@users.noreply.github.com>
2025-09-08 19:25:47 -0400

    Update organelle.py

--- organelle.py
+++ organelle.py
@@ -16,5 +16,14 @@ class Organelle():
                              color = "white")
             ax.add_artist(obj)
 
+def draw(self, ax, position=(0,0,0)):
+        if self.shape == "circle" and self.radius:
+            u = np.linspace(0, 2*np.pi, 20)
+            v = np.linspace(0,np.pi,20)
+            x = self.radius * np.outer(np.cos(u), np.sin(v)) + position[0]
+            y = self.radius * np.outer(np.sin(u), np.sin(v)) + position[1]
+            z = self.radius * np.outer(np.ones_like(u), np.cos(v)) + position[2]
+            ax.plot_surface(x,y,z, color="white" if self.name=="nucleus" else "lightblue", alpha = 0.6)
+
 
     
