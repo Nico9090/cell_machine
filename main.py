de11b32e26d31e27a53800f0cf8db299211fe6c4
Nico9090 <161892855+Nico9090@users.noreply.github.com>
2025-09-08 21:57:32 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,37 @@
 from cell import Cell
-from grow import Grow
 from organelle import Organelle
+import matplotlib.pyplot as plt
+import numpy as np
+def showObjectInfo(obj):
+    return {
+        "objType":type(obj),
+        "objAttrMethods":dir(obj),
+        "objInst":obj.__dict__,
+        "objStruc":vars(obj)
+    }
 
-epithelial_cell=Grow()
-print(epithelial_cell.define_cell(5,3,10,8,-10,-8))
-#print("Surface area: ", epithelial.calculate_cell_surface_area())
-#print("Volume: ", epithelial.calculate_volume())
+'''    
+epithelialCell = Cell()
+epithelialCell.makeCell(4,5)
+nucleus = Organelle(name = "nucleus",
+                    shape = "circle",
+                    radius = 0.5)
+
+epithelialCell.addOrganelle(nucleus)
+
+epithelialCellattr = showObjectInfo(epithelialCell)
+print(epithelialCellattr["objStruc"])
+epithelialCell.drawCell()
+'''
+epithelialCell = Cell()
+epithelialCell.makeCell(4, 8)
+
+nucleus = Organelle(name="nucleus", shape="spherical", radius=3)
+epithelialCell.addOrganelle(nucleus)
+
+epithelialCell.drawCell()
+
+# Now you can interact with all geometry data
+for g in epithelialCell.geometries:
+    print(f"shape={g.x.shape}, center≈({g.x.mean():.2f},{g.y.mean():.2f},{g.z.mean():.2f})")
 
